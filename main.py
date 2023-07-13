59f924329f4dc90c31ab8f83560b9eb85b79ac79
LinglinChangSmith <127280154+LinglinChangSmith@users.noreply.github.com>
2023-07-12 21:19:11 -0700

    import all applications needed

---
+++ main.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+# Terminal: pip3 install -U Flask-SQLAlchemy
+from flask_bootstrap import Bootstrap
+# Terminal: pip3 install flask-bootstrap
