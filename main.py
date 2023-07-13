b9f19ceb06474c468a782f11ad8fe93b29198775
LinglinChangSmith <127280154+LinglinChangSmith@users.noreply.github.com>
2023-07-12 22:05:58 -0700

    create database

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ app = Flask(__name__)
 app.app_context().push()
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///chores.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Chores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(50), unique=True, nullable=False)
+    frequency = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+
+
+db.create_all()
+
