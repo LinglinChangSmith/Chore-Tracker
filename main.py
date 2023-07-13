9dc67f97011102eabad677fb1b99e1c4939d71ed
LinglinChangSmith <127280154+LinglinChangSmith@users.noreply.github.com>
2023-07-12 21:22:00 -0700

    Created Flask app

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 # Terminal: pip3 install -U Flask-SQLAlchemy
 from flask_bootstrap import Bootstrap
 # Terminal: pip3 install flask-bootstrap
+
+
+app = Flask(__name__)
+app.app_context().push()
+Bootstrap(app)
+
